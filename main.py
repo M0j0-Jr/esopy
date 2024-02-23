ffc1d7e8fde54731dd3d8553503e4f0dc77cd135
GlebyGlobster <57631406+M0j0-Jr@users.noreply.github.com>
2024-02-23 14:26:33 -0600

    added esopy again
    for real this time

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import decoder
+import dontEdit
+
+# isopy consisted of 4 character strings with 4 possible letters (a, b, c, and d) with 
+# different ascii characters assigned unique 4 letter character strings.
+
+# run main.py to test your isopy script.
+# it will automatically decode the isopy script written in codeHere.py using decoder.py,
+# and then main.py will run the decoded isopy script in dontEdit.py. 
+
+# if you want to find a character string for a specific letter, refer to glossary.txt.
